61b234742c08927d9806a030970b5b5ed780057f
maxkolt <12345kolt@gmail.com>
2025-01-13 14:51:29 +0300

    init

--- client/src/components/ProductItem/ProductItem.jsx
+++ client/src/components/ProductItem/ProductItem.jsx
@@ -2,21 +2,25 @@ import React from 'react';
 import Button from "../Button/Button";
 import './ProductItem.css';
 
-const ProductItem = ({product, className, onAdd}) => {
-
+const ProductItem = ({ product, className, onAdd }) => {
   const onAddHandler = () => {
     onAdd(product);
-  }
+  };
 
   return (
-    <div className={'product ' + className}>
-      <div className={'img'} {product.img}/>
-      <div className={'title'}>{product.title}</div>
-      <div className={'description'}>{product.description}</div>
-      <div className={'price'}>
+    <div className={`product ${className}`}>
+      {/* Отображение изображения, если оно есть */}
+      {product.img && (
+        <div className="img">
+          <img src={product.img} alt={product.title} />
+        </div>
+      )}
+      <div className="title">{product.title}</div>
+      <div className="description">{product.description}</div>
+      <div className="price">
         <span>Стоимость: <b>{product.price}</b></span>
       </div>
-      <Button className={'add-btn'} onClick={onAddHandler}>
+      <Button className="add-btn" onClick={onAddHandler}>
         Добавить в корзину
       </Button>
     </div>
--- client/src/components/ProductList/ProductList.jsx
+++ client/src/components/ProductList/ProductList.jsx
@@ -1,81 +1,30 @@
-import React, {useState} from 'react';
-import './ProductList.css';
-import ProductItem from "../ProductItem/ProductItem";
-import {useTelegram} from "../../hooks/useTelegram";
-import {useCallback, useEffect} from "react";
+import React, { useEffect, useState } from 'react';
 
-const products = [
-  {id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
-  {id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая'},
-  {id: '3', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые'},
-
-]
-
-const getTotalPrice = (items = []) => {
-  return items.reduce((acc, item) => {
-    return acc += item.price
-  }, 0)
-}
-
-const ProductList = () => {
-  const [addedItems, setAddedItems] = useState([]);
-  const {tg, queryId} = useTelegram();
-
-  const onSendData = useCallback(() => {
-    const data = {
-      products: addedItems,
-      totalPrice: getTotalPrice(addedItems),
-      queryId,
-    }
-    fetch('http://85.119.146.179:8000/web-data', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data)
-    })
-  }, [addedItems])
+function ProductList() {
+  const [ads, setAds] = useState([]);
 
   useEffect(() => {
-    tg.onEvent('mainButtonClicked', onSendData)
-    return () => {
-      tg.offEvent('mainButtonClicked', onSendData)
-    }
-  }, [onSendData])
-
-  const onAdd = (product) => {
-    const alreadyAdded = addedItems.find(item => item.id === product.id);
-    let newItems = [];
-
-    if(alreadyAdded) {
-      newItems = addedItems.filter(item => item.id !== product.id);
-    } else {
-      newItems = [...addedItems, product];
-    }
-
-    setAddedItems(newItems)
-
-    if(newItems.length === 0) {
-      tg.MainButton.hide();
-    } else {
-      tg.MainButton.show();
-      tg.MainButton.setParams({
-        text: `Купить ${getTotalPrice(newItems)}`
-      })
-    }
-  }
+    // Запрос к вашему серверу Express
+    fetch('http://localhost:8000/api/ads') // Или замените localhost на ваш продакшн сервер
+      .then((response) => response.json())
+      .then((data) => setAds(data))
+      .catch((err) => console.error('Ошибка загрузки объявлений:', err));
+  }, []);
 
   return (
-    <div className={'list'}>
-      {products.map(item => (
-        <ProductItem
-          product={item}
-          onAdd={onAdd}
-          className={'item'}
-        />
-      ))}
+    <div>
+      <h1>Каталог объявлений</h1>
+      <ul>
+        {ads.map((ad, index) => (
+          <li key={index}>
+            <strong>Категория:</strong> {ad.category} <br />
+            <strong>Описание:</strong> {ad.description} <br />
+            <small>Добавлено: {new Date(ad.createdAt).toLocaleString()}</small>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-};
+}
 
 export default ProductList;
